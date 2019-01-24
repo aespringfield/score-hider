0dc11742b5983e453e67f51cd6da394fd2de7c99
Anna Springfield <anna.e.springfield@gmail.com>
2019-01-23 21:38:28 -0600

    Clean up script execution

--- hideScores.js
+++ hideScores.js
@@ -1,20 +1,12 @@
 document.getElementById('hideScores').onclick = function(event) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.executeScript(
+            {code: `document.querySelectorAll('.imspo_mt__t-sc, svg.imspo_mt__triangle').forEach((node) => {
+                node.style.display = 'none';
+            })`}
+        );
+        chrome.tabs.executeScript(
             {file: './highlightGames.js'}
         );
     });
 }
-
-// chrome.tabs.executeScript(
-//     {file: './highlightGames.js'}
-// );
-
-// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     chrome.tabs.executeScript(
-//         {code: `document.querySelectorAll('.imspo_mt__t-sc').forEach((node) => {
-//             node.style.display = 'none';
-//         })`
-//         }
-//     );
-// });
